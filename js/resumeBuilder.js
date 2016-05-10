d57ab13cb0e4f0bbe32fb7da3baea231d11ec6f8
Gil Roman <gil@digitalgil.com>
2016-05-10 10:17:29 -0400

    Correct resumeBuilder.js and style.css per review notes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,7 +9,9 @@ var bio = {
 		"github" : "gilroman",
 		"location" : "Brooklyn"
 	},
-	"welcomeMessage" : "Hi! I'm Gil, a front end developer based in Brooklyn, NY.  I enjoy working in teams, communicating clearly and taking on projects with meaning.",
+	"welcomeMessage" : "Hi! I'm Gil, a front end developer based in Brooklyn, NY. " +
+										  "I enjoy working in teams, communicating clearly and" +
+										  "taking on projects with meaning.",
 	"skills" : ["HTML", "CSS", "Bootstrap", "Javascript", "JQuery"],
 	"biopic" : "images/haleakala.png",
 	"display" : function() {
@@ -55,14 +57,17 @@ var work = {
 		"title" : "Studio Director",
 		"location" : "New York",
 		"dates" : "2003 - Present",
-		"description" : "Managed the technical and daily business needs of the studio. Implemented an hourly billing solution and other solutions to spreadsheet and paper based processes."
+		"description" : "Managed the technical and daily business needs of the" +
+										"studio. Implemented an hourly billing solution and other "+
+										"solutions to spreadsheet and paper based processes."
 		},
 		{
 		"employer" : "Sullivan and Cromwell",
 		"title" : "Paralegal",
 		"location" : "New York",
 		"dates" : "2002-2003",
-		"description" : "Assisted attorneys in the discovery process and the management of legal case exhibits of various legal cases."
+		"description" : "Assisted attorneys in the discovery process and the" +
+										"management of legal case exhibits of various legal cases."
 		},
 		{
 		"employer" : "Bear Stearns",
@@ -72,19 +77,17 @@ var work = {
 		"description" : "Mantained the internal trading system's data set of national payment data."
 		}],
 		"display" : function(){
-			for (job in work.jobs){
-    		if (work.hasOwnProperty("jobs")) {
-    			var formatedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-    			var formatedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-    			var formatedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-    			var formatedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-    			var formatedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-    			$("#workExperience").append(HTMLworkStart);
-    			$(".work-entry:last").append(formatedEmployer+formatedWorkTitle);
-    			$(".work-entry:last").append(formatedDates);
-    			$(".work-entry:last").append(formatedWorkLocation);
-    			$(".work-entry:last").append(formatedWorkDescription);
-    		}
+			for (var i = 0; i<work.jobs.length; i++){
+    		var formatedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+    		var formatedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+    		var formatedDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
+    		var formatedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
+    		var formatedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+    		$("#workExperience").append(HTMLworkStart);
+    		$(".work-entry:last").append(formatedEmployer+formatedWorkTitle);
+    		$(".work-entry:last").append(formatedDates);
+    		$(".work-entry:last").append(formatedWorkLocation);
+    		$(".work-entry:last").append(formatedWorkDescription);
     	}
 		}
 };
@@ -98,32 +101,30 @@ var projects = {
 		"title" : "Sample Project 1",
 		"dates" : "2015",
 		"description" : "loreimpsu",
-		"pictures" : ["images/197x148.gif","images/197x148.gif"]
+		"images" : ["images/197x148.gif","images/197x148.gif"]
 	},
 	{
 		"title" : "Sample Project 2",
 		"dates" : "2016",
 		"description" : "loreimpsu",
-		"pictures" : ["images/197x148.gif","images/197x148.gif"]
+		"images" : ["images/197x148.gif","images/197x148.gif"]
 	}],
 		"display" : function(){
-			for (project in projects.projects){
-				if (projects.hasOwnProperty("projects")){
+			for (var i = 0; i<projects.projects.length; i++){
 				$("#projects").append(HTMLprojectStart);
-  			var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-  			var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-  			var formattedProjectDesc = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
-  			var formattedProjectPic = HTMLprojectImage.replace("%data%", projects.projects[project].pictures);
+  			var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
+  			var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
+  			var formattedProjectDesc = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+  			var formattedProjectPic = HTMLprojectImage.replace("%data%", projects.projects[i].images);
   			$(".project-entry:last").append(formattedProjectTitle);
   			$(".project-entry:last").append(formattedProjectDates);
   			$(".project-entry:last").append(formattedProjectDesc);
 
 				//Display array of project images
-				projects.projects[project].pictures.forEach(function(val){
-				var formattedProjectPic = HTMLprojectImage.replace("%data%", val);
-				$(".project-entry:last").append(formattedProjectPic);
+				projects.projects[i].images.forEach(function(val){
+					var formattedProjectPic = HTMLprojectImage.replace("%data%", val);
+					$(".project-entry:last").append(formattedProjectPic);
 				});
-  			}
   		}
   	}
 };
@@ -136,7 +137,7 @@ var education = {
 	{
 	"name" : "CUNY Baruch College",
 	"location" : "New York",
-	"dates" : 2001,
+	"dates" : "2001",
 	"degree" : "BBA",
 	"majors" : ["Computer Information Systems"],
 	"url" : "http://www.cuny.edu"
@@ -145,11 +146,11 @@ var education = {
 	{
 		"title" : "Front End Developer Nanodegree" ,
 		"school" : "Udacity",
-		"dates" : 2016,
+		"date" : "2016",
 		"url" : "http://www.udacity.com"
 	}],
 	"display" : function(){
-		for (school in education.schools){
+		for (var school in education.schools){
 			if (education.hasOwnProperty("schools")){
 				$("#education").append(HTMLschoolStart);
 				var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
@@ -167,13 +168,14 @@ var education = {
 			}
 		}
 
-		for (onlineCourse in education.onlineCourses){
+		$("#education").append(HTMLonlineClasses);
+
+		for (var onlineCourse in education.onlineCourses){
 			if (education.hasOwnProperty("onlineCourses")) {
-				$("#education").append(HTMLonlineClasses);
 				$("#education").append(HTMLschoolStart)
 				var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineCourse].title);
 				var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[onlineCourse].school);
-				var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[onlineCourse].dates);
+				var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[onlineCourse].date);
 				var formattedOnlineUrl = HTMLonlineURL.replace("%data%", education.onlineCourses[onlineCourse].url);
 				$(".education-entry:last").append(formattedOnlineTitle+formattedOnlineSchool);
 				$(".education-entry:last").append(formattedOnlineDates);
